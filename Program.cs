3d0ae6e55b9a949a042058af5637238c0012c132
Viney <vineykhaneja999@gmail.com>
2022-02-12 15:25:43 +0530

    [Viney] FINAL FEATURE ADDED: 2 Players Game

--- Program.cs
+++ Program.cs
@@ -8,47 +8,58 @@ namespace Game
 {
     internal class Program
     {
+        //CONSTANTS
+        const int SNAKE = 1;
+        const int LADDER = 2;
+        const int TARGETPOSITION = 100;
+        public static int Getposition(int position)
+        {
+            StartAgain:
+
+            //VARIABLES
+            Random random = new Random();
+            int chooseOption = random.Next(0, 3);
+            int dieRoll = random.Next(1, 7);
+
+            switch (chooseOption)
+            {
+                case SNAKE:
+                    position -= dieRoll;
+                    int output1 = position < 0 ? position += dieRoll : position;
+                    break;
+                case LADDER:
+                    position += dieRoll;
+                    Console.WriteLine("Ladder wons");
+                    //For decrementing if player crosses 100 position.
+                    int output2 = position > TARGETPOSITION ? position -= dieRoll : position;
+                    //For playing again, if player got at ladder
+                    goto StartAgain;
+                default:
+                    position += 0;
+                    Console.WriteLine("NOT PLAYED");
+                    break;
+            }
+        return position;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake & Ladder game");
-            //CONSTANTS
-            const int SNAKE = 1;
-            const int LADDER = 2;
-            const int TARGETPOSITION = 100;
 
             //VARIABLES
-            int position1 = 0;
-            Random random = new Random();
-            int counterDieRoll = 0;
-            
-            while (position1 != TARGETPOSITION)
+            int player1Position = 0;
+            int player2Position = 0;
+
+            while (player1Position != TARGETPOSITION && player2Position != TARGETPOSITION)
             {
-                //VARIABLES
-
-                int dieRoll = random.Next(1, 7);
-                int chooseOption = random.Next(0, 3);
-
-                switch (chooseOption)
-                {
-                    case SNAKE:
-                        position1 -= dieRoll;
-                        int output1 = position1 < 0 ? position1 += dieRoll : position1;
-                        Console.WriteLine($"Player is at {position1}");
-                        break;
-                    case LADDER:
-                        position1 += dieRoll;
-                        //For decrementing if player crosses 100 position.
-                        int output2 = position1 > TARGETPOSITION ? position1 -= dieRoll : position1;
-                        Console.WriteLine($"Player is at {position1}");
-                        break;
-                    default:
-                        position1 += 0;
-                        Console.WriteLine("NOT PLAY");
-                        break;
-                }
-            counterDieRoll++;
+                player1Position = Getposition(player1Position);
+                player2Position = Getposition(player2Position);
+                Console.WriteLine($"PLAYER1 IS AT {player1Position}");
+                Console.WriteLine($"PLAYER2 IS AT {player2Position}");
             }
-            Console.WriteLine($"Game over ! Player is at exact {position1}.\nTotal number of die roll is {counterDieRoll}.");
+            string result = player1Position == TARGETPOSITION ? ("Player1 Won the game") : ("Player2 won the game");
+            Console.WriteLine(result);
+            
+            
         }
     }
 }
